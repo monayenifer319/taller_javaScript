8c79010e004c522014a4c6ac072b2d59cdc76176
yenifer valencia <mona_319@hotmail.es>
2017-07-21 09:57:51 -0500

    finalizacion del punto 16

--- public_html/p16/js/codigo.js
+++ public_html/p16/js/codigo.js
@@ -1,1 +1,4 @@
-console.log("Hola, este es mi primer script en JavaScript");
+var laA = Number(prompt("Enter the side a"));
+var laB = Number(prompt("Enter the side b"));
+perim = (laA*2) + (laB*2);
+console.log("The perimeter of the triangle is: "+ perim);
