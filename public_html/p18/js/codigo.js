4c37082d08f9c2e79d5b112b07e926248936d079
yenifer valencia <mona_319@hotmail.es>
2017-07-21 22:14:43 -0500

    finalizacion punto 18

--- public_html/p18/js/codigo.js
+++ public_html/p18/js/codigo.js
@@ -1,6 +1,6 @@
 var salary = Number(prompt("enter the salary"));
 var retention = Number(prompt("Enter the percentage of the retention Thus: 0.05, if it is 5%, thus 0.50 if it is 50%"));
-var valReten = salary * valReten;
+var valReten = salary * retention;
 var payment = salary - valReten;
 console.log("The value of the retention is: "+ valReten);
 console.log("The total payment is: "+ payment);
