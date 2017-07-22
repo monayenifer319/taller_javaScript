217597181227189977d1f497cf25c082337a111d
yenifer valencia <mona_319@hotmail.es>
2017-07-21 22:09:51 -0500

    punto 18 dificultad para sacar el porcentaje

--- public_html/p18/js/codigo.js
+++ public_html/p18/js/codigo.js
@@ -1,1 +1,6 @@
-console.log("Hola, este es mi primer script en JavaScript");
+var salary = Number(prompt("enter the salary"));
+var retention = Number(prompt("Enter the percentage of the retention Thus: 0.05, if it is 5%, thus 0.50 if it is 50%"));
+var valReten = salary * valReten;
+var payment = salary - valReten;
+console.log("The value of the retention is: "+ valReten);
+console.log("The total payment is: "+ payment);
