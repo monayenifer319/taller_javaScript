b3519cd29afe8a6f7b1629464b2b60052720f77f
yenifer valencia <mona_319@hotmail.es>
2017-07-22 02:26:25 -0500

    punto 21 dificultad: sacar la raiz cuadrada

---
+++ public_html/p21/js/codigo.js
@@ -0,0 +1,9 @@
+var laA = Number(prompt("Insert the side leg to (a)"));
+var laB = Number(prompt("Insert the side leg to (b)"));
+var h = (laA **2) + (laB**2);
+var answer = 
+console.log("Linked to (a): = "+laA);
+console.log("Linked to (b): = "+laB);
+console.log("so: = "+laA+ " ^  2  + "+laB + " ^ 2 = "+h);
+console.log("The hypotenuse is: "+h);
+
