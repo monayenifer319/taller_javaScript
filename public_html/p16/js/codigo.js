d3604ca1b1daf8dfa898136b6faa96499b6f5fce
yenifer valencia <mona_319@hotmail.es>
2017-07-21 21:23:23 -0500

    finalizacion del punto 17

--- public_html/p16/js/codigo.js
+++ public_html/p16/js/codigo.js
@@ -1,4 +1,9 @@
 var laA = Number(prompt("Enter the side a"));
 var laB = Number(prompt("Enter the side b"));
-perim = (laA*2) + (laB*2);
-console.log("The perimeter of the triangle is: "+ perim);
+var base = Number(prompt("Enter base"));
+var height = Number(prompt("Enter height"));
+var c = (laA**2) + (laB**2);
+//var perim = 
+var area = (base * height)/2;
+console.log("The perimeter of the triangle is: "+ c);
+console.log("The area of the triangle is: "+ area);
--- public_html/p17/js/codigo.js
+++ public_html/p17/js/codigo.js
@@ -1,1 +1,6 @@
-console.log("Hola, este es mi primer script en JavaScript");
+console.log("Program that solves a rule of three");
+var a = Number(prompt("Enter the First number"));
+var b = Number(prompt("Enter the second number"));
+var c = Number(prompt("Enter the Third number"));
+answer = (c*b)/a; 
+console.log("the answer is: "+ answer);
