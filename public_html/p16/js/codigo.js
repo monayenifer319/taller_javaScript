8151f3f734f526ea4588877d194b3225fa633925
yenifer valencia <mona_319@hotmail.es>
2017-07-25 22:09:35 -0500

    finlizacion del punto 24

--- public_html/p16/js/codigo.js
+++ public_html/p16/js/codigo.js
@@ -1,9 +1,9 @@
 var laA = Number(prompt("Enter the side a"));
 var laB = Number(prompt("Enter the side b"));
+var laC = Number(prompt("Enter the side c"));
 var base = Number(prompt("Enter base"));
 var height = Number(prompt("Enter height"));
-var c = (laA**2) + (laB**2);
-//var perim = 
+var perim = laA+laB+laC;
 var area = (base * height)/2;
-console.log("The perimeter of the triangle is: "+ c);
+console.log("The perimeter of the triangle is: "+ perim);
 console.log("The area of the triangle is: "+ area);
--- public_html/p24/js/codigo.js
+++ public_html/p24/js/codigo.js
@@ -3,3 +3,15 @@ var hours = Number(prompt("Enter the number of hours"));
 var day = 24;
 // una semana es igual a 168 horas
 var week = 168;
+    if((hours >= day) && (hours < week)){
+       var answer = hours / day;
+       var answerWe = hours/week;
+        console.log("The hours entered belong to: "+answer+ " day ");
+        console.log("Infested hours belong to less than one weeks: "+answerWe);
+    }else{
+        var answer = hours / day;
+            var answerWe = hours/week;
+            console.log("The hours entered belong to: "+answer+ " day ");
+            console.log("The hours entered belong to weeks: "+answerWe);
+    }
+    
