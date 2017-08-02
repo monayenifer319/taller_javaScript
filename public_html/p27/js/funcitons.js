729205346a30e78188d5e5f8f82dc5ca34b256f3
yenifer valencia <mona_319@hotmail.es>
2017-08-02 15:35:51 -0500

    finalizacion punto 27

--- public_html/p27/js/funcitons.js
+++ public_html/p27/js/funcitons.js
@@ -19,14 +19,12 @@ function family() {
         name: " ",
         lastname: " ",
         id: " ",
-        phone: " ",
-        email: " "
+        phone: " "
     };
     regfamily.name = prompt("Enter nomber");
     regfamily.lastname = prompt("Enter lastname");
     regfamily.id = prompt("Enter id number");
     regfamily.phone = prompt("Enter number of phone");
-    regfamily.email = proompt("Enter mail");
     return regfamily;
 }
 
@@ -62,15 +60,34 @@ function print(regperso, regfamily, regjob, regprofile) {
     console.log(regperso);
     console.log("CV");
     console.log(" ");
-    console.log("personal information");
+    console.log("*********personal information***********");
     console.log("name: " + regperso.name);
     console.log("lastname: " + regperso.lastname);
     console.log("id number: " + regperso.id);
     console.log("number phone: " + regperso.phone);
+    console.log("email: " + regperso.email);
+    console.log("***********Family Reference**********");
+    console.log(" ");
+    console.log("name: " + regfamily.name);
+    console.log("lastname: " + regfamily.lastname);
+    console.log("id number: " + regfamily.id);
+    console.log("number phone: " + regfamily.phone);
+    console.log("*********** Reference job**********");
+    console.log(" ");
+    console.log("name: " + regjob.name);
+    console.log("lastname: " + regjob.lastname);
+    console.log("id number: " + regjob.id);
+    console.log("number phone: " + regjob.phone);
+    console.log("campany phone: " + regjob.campany);
+    console.log("*********** professional profile **********");
+    console.log(" ");
+    console.log("prophecyor ocupation: " + regprofile.ocupation);
+    console.log("obtained titles: " + regprofile.titles);
+    console.log("years of work experience: " + regprofile.experience);
 }
 
 //var prue;
 function menu() {
     var op = parseInt(prompt("Menu:\n 1. Personal information\n 2. Family Reference\n 3. Reference job\n 4. professional profile\n 5. Print resume\n 0. Exit the program"));
     return op;
-}
+} 
