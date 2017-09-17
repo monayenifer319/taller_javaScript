e2b5e842f9c9bd08a5c0381bcf0029ce6e9b7be8
yenifer valencia <mona_319@hotmail.es>
2017-09-17 01:30:57 -0500

    finalizacion del puntto 39

--- public_html/p40/js/codigoP4.js
+++ public_html/p40/js/codigoP4.js
@@ -1,19 +1,4 @@
-var name = prompt("enter your name");
-var id = prompt("Enter type of dicument");
-var numberid = prompt("Enter the ID number");
-var gender = prompt("Enter your gender");
-var height = prompt("enter your height");
-var live = prompt("Is it true that I live in Carthage?");
-
-console.log("hello my name is: "+ name);
-console.log("number of " + id + " is: " + numberid);
-console.log("gender " + gender);
-console.log("height is: " + height);
-
-if(live === "si"){
-    console.log("true");
-}else{
-    console.log("false");
-}
-
+$(document).ready(function () {
+  $('#message').text('“Hola, este es mi script número 38 en JavaScript almacenado en una variable usando jQuery.”');
+});
 
